8e66e7b970ccbe664060226957212939d040dcc2
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-11-08 01:08:08 -0800

    componentized cat imags getting ready for virtualization

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/get-cat-pictures')
+const { data: cat_pictures } = await useFetch('/api/get-cat-pictures')
 </script>
 
 <template>
   <div class="flex flex-col space-y-5 max-w-xl mx-auto my-5 items-center">
-    <img v-for="cat_picture in data" :key="cat_picture.reddit_id" :src="cat_picture.media" :alt="cat_picture.title"
-      class="w-3/4 h-auto rounded-md" />
+    <CatImg v-for="cat_picture in cat_pictures" :key="cat_picture.reddit_id" :reddit_id="cat_picture.reddit_id"
+      :title="cat_picture.title" :media="cat_picture.media" :source="cat_picture.source" />
   </div>
 </template>
---
+++ components/CatImg.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    reddit_id: string;
+    media: string;
+    source: string;
+    title: string;
+}>()
+</script>
+
+<template>
+    <img :src="media" :alt="title" class="w-3/4 h-auto rounded-md" />
+</template>
