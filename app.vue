6ccb78bea8b30b132e14a82cf142d9b1bd394fc3
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-11-07 22:54:26 -0800

    changed to solo column view

--- app.vue
+++ app.vue
@@ -3,8 +3,8 @@ const { data } = await useFetch('/api/get-cat-pictures')
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-5">
+  <div class="flex flex-col space-y-5 max-w-xl mx-auto my-5 items-center">
     <img v-for="cat_picture in data" :key="cat_picture.id" :src="cat_picture.media" :alt="cat_picture.title"
-      class="w-auto h-auto" />
+      class="w-3/4 h-auto rounded-md" />
   </div>
 </template>
