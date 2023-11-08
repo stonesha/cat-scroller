b9ae679422815b7aa5761afa52b5871f47685b7e
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-11-08 01:02:14 -0800

    added page param to endpoint

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ const { data } = await useFetch('/api/get-cat-pictures')
 
 <template>
   <div class="flex flex-col space-y-5 max-w-xl mx-auto my-5 items-center">
-    <img v-for="cat_picture in data" :key="cat_picture.id" :src="cat_picture.media" :alt="cat_picture.title"
+    <img v-for="cat_picture in data" :key="cat_picture.reddit_id" :src="cat_picture.media" :alt="cat_picture.title"
       class="w-3/4 h-auto rounded-md" />
   </div>
 </template>
--- server/api/get-cat-pictures.ts
+++ server/api/get-cat-pictures.ts
@@ -2,11 +2,24 @@ import { db } from "~/db";
 import { cat_pictures } from "~/db/schema";
 import { desc } from "drizzle-orm";
 
-export default defineEventHandler(() => {
+export default defineEventHandler((event) => {
+  let page: number;
+  if (!event.context.params) page = 0;
+  else page = parseInt(event.context.params.offset) as number;
+
+  const offset = (page - 1) * 25;
+
   const results = db
-    .select()
+    .select({
+      reddit_id: cat_pictures.reddit_id,
+      title: cat_pictures.title,
+      media: cat_pictures.media,
+      source: cat_pictures.source,
+    })
     .from(cat_pictures)
     .orderBy(desc(cat_pictures.created_at))
-    .limit(25);
+    .limit(25)
+    .offset(offset);
+
   return results;
 });
